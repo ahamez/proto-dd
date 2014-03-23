850c4d078c07d3b7d6b4d1b20ddee7bb82c17883
Alexandre Hamez <alexandre.hamez@gmail.com>
2014-03-23 12:01:45 +0100

    default_sparse_value -> default_value

--- sdd/dd/default_value.hh
+++ sdd/dd/default_value.hh
@@ -1,15 +1,18 @@
-#ifndef _SDD_DD_SPARSE_VALUE_HH_
-#define _SDD_DD_SPARSE_VALUE_HH_
+#ifndef _SDD_DD_DEFAULT_VALUE_HH_
+#define _SDD_DD_DEFAULT_VALUE_HH_
 
 namespace sdd { namespace dd {
 
 /*------------------------------------------------------------------------------------------------*/
 
 template <typename T>
-struct sparse_value;
+struct default_value
+{
+  static T value() noexcept(noexcept(T())) {return T();}
+};
 
 /*------------------------------------------------------------------------------------------------*/
 
 }} // namespace sdd::dd
 
-#endif // _SDD_DD_SPARSE_VALUE_HH_
+#endif // _SDD_DD_DEFAULT_VALUE_HH_
